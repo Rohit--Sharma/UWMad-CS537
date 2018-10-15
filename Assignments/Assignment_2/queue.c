d4c6317b38ea0fbaad9b6e71b71ce54b7e71d767
Rohit <rohit.sharma@wisc.edu>
2018-10-15 00:35:48 -0500

    Added some prints for debugging

--- Assignments/Assignment_2/queue.c
+++ Assignments/Assignment_2/queue.c
@@ -12,14 +12,14 @@ Queue* createQueue(int capacity)
     struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
     queue->size = capacity; 
     queue->no_of_elements = 0;
-    queue->head = 0;  
-    queue->tail = 0;
+    queue->head = -1;  
+    queue->tail = -1;
     queue->enqueueCount = 0;
     queue->dequeueCount = 0;
     queue->enqueueBlockCount = 0;
     queue->dequeueBlockCount = 0;
-    for (int i=0; i<capacity; i++)    
-	queue->string[i] = NULL;
+    for (int i = 0; i < capacity; i++)    
+	    queue->string[i] = NULL;
     sem_init(&(queue->mutex), 0, 1);
     sem_init(&(queue->full), 0, 0);
     sem_init(&(queue->empty), 0, QUEUE_SIZE);
@@ -27,19 +27,31 @@ Queue* createQueue(int capacity)
 }
 
 void EnqueueString(Queue* queue, char *string) 
-{ 
+{
+    sem_wait(&(queue->mutex));
+    printf("Head enqueue before: %d\n", queue->head);
+    sem_post(&queue->mutex);
     if (queue->size == queue->no_of_elements){
 	    queue->enqueueBlockCount++;
     }
     sem_wait(&(queue->empty)); 
     sem_wait(&(queue->mutex));
-    queue->tail = (queue->tail + 1) % queue->size; 
+    if (queue->no_of_elements==0)
+    {
+        queue->head = 0;
+        queue->tail = 0;
+        queue->no_of_elements = 0;
+    }
     queue->string[queue->tail] = string; 
-    queue->no_of_elements = queue->no_of_elements + 1;
+    queue->tail = (queue->tail + 1) % queue->size;
+    queue->no_of_elements++;
     queue->enqueueCount++;
     sem_post(&queue->mutex);
     sem_post(&queue->full);
+    sem_wait(&(queue->mutex));
+    printf("Head enqueue after: %d\n", queue->head);
     printf("String has been enqueued to queue: %x, head = %d and tail = %d, no_of_elements = %d\n", queue, queue->head, queue->tail, queue->no_of_elements); 
+    sem_post(&queue->mutex);
 }
 
 char* DequeueString(Queue* queue) 
@@ -51,11 +63,11 @@ char* DequeueString(Queue* queue)
     sem_wait(&queue->mutex);
     char* queue_string = queue->string[queue->head];
     queue->head = (queue->head + 1) % queue->size; 
-    queue->no_of_elements = queue->no_of_elements - 1;
+    queue->no_of_elements--;
     queue->dequeueCount++;
+    printf("String has been dequeued from queue: %x, head = %d and tail = %d, no_of_elements = %d\n", queue, queue->head, queue->tail, queue->no_of_elements);  
     sem_post(&queue->mutex);
     sem_post(&queue->empty);
-    printf("String has been dequeued from queue, head = %d and tail = %d, no_of_elements = %d\n", queue->head, queue->tail, queue->no_of_elements);  
     return queue_string;
 }
 
--- Assignments/Assignment_2/thread.c
+++ Assignments/Assignment_2/thread.c
@@ -13,7 +13,9 @@ void *reader(void *out_queue)
     size_t max_line_len = MAX_LINE_LEN + 1;
     while (getline(&line, &max_line_len, stdin) != -1) {
         printf("Reading: %s", line);
+        printf("Head before: %d\n", ((Queue *)out_queue)->head);
         EnqueueString((Queue *)out_queue, line);
+        printf("Head after: %d\n", ((Queue *)out_queue)->head);
     }
     return NULL;
 }
@@ -23,10 +25,14 @@ void *munch1(void *queues)
     char *string;
     string = DequeueString(((pthread_param *)queues)->input);
     
+    printf("Before Munch1: %s", string);  
+
     for (int i=0; string[i] != '\0'; i++) {
         if (string[i] == ' ')
             string[i] = '*';
     }
+
+    printf("After Munch1: %s", string);
 
     EnqueueString(((pthread_param *)queues)->output, string);
     return NULL;
@@ -37,9 +43,14 @@ void *munch2(void *queues)
     char *string;
     string = DequeueString(((pthread_param *)queues)->input);
     
+    printf("Before Munch2: %s", string);
+
     for (int i=0; string[i] != '\0'; i++) {
-	    string[i] = toupper(string[i]);
+        if (islower(string[i]))
+    	    string[i] = toupper(string[i]);
     }
+
+    printf("After Munch2: %s", string);
 
     EnqueueString(((pthread_param *)queues)->output, string);
     return NULL;
