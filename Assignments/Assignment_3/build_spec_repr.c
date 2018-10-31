af92c1ca81eec32bd07169df1d352459dad70872
Rohit <rohit.sharma@wisc.edu>
2018-10-30 21:56:01 -0500

    Added draft code to parse the makefile. Added methods to create a node and display it in build_spec_repr.c Added a test file in the test_inputs dir

--- Assignments/Assignment_3/build_spec_repr.c
+++ Assignments/Assignment_3/build_spec_repr.c
@@ -1,4 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
+//
 // Authors:         Rohit Kumar Sharma, M. Giri Prasanna
 // NetID:           rsharma54, mugundakrish
 // CSLogin:         rsharma, mgiriprasanna
@@ -6,3 +7,24 @@
 // Created on:      October 25, 2018
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include "build_spec_graph.h"
+
+MakeNode * create_node (char * target, char * command) {
+    MakeNode * makenode = (MakeNode *) malloc (sizeof(MakeNode));
+
+    makenode->name = target;
+    makenode->rule = command;
+    makenode->isLeafNode = 0;
+    makenode->children = NULL;
+
+    return makenode;
+}
+
+void display_node (MakeNode * makenode) {
+    if (makenode != NULL)
+        fprintf(stdout, "MTarget: %s\nMCommand: %s\n", makenode->name, makenode->rule);
+    else
+        fprintf(stderr, "Error: Cannot display makenode. makenode is NULL\n");
+}
--- Assignments/Assignment_3/text_parsing.c
+++ Assignments/Assignment_3/text_parsing.c
@@ -10,9 +10,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "build_spec_graph.h"
 
-char **read_input_makefile (char *file_name) {
-    char *input = "main.o: main.c main.h dep1.c";
+extern const int MAX_LINE_LEN;
+
+char ** tokenize_string (char *input) {
 
     char **result = NULL;
     char *temp = input;
@@ -21,9 +23,12 @@ char **read_input_makefile (char *file_name) {
     int count_tokens = 0;   // count_tokens will have the number of tokens in the input string + 1 for indicating the end
     while (*(temp + i) != '\0') {
         if (*(temp + i) == ' ') {
-            // TODO: Ignore all the continuous white spaces
             count_tokens++;
-            last_space = temp + i;
+            // Ignore all the continuous white spaces
+            while (*(temp + i) == ' ') {
+                last_space = temp + i;
+                i++;
+            }
         }
         i++;
     }
@@ -47,28 +52,107 @@ char **read_input_makefile (char *file_name) {
             i++;
             j++;
         }
+        // discard continuous spaces
+        while (*(temp + i) == ' ') {
+            i++;
+        }
+
         *(token + j) == '\0';
-        *(result + idx++) = strdup(token);
+        *(result + idx++) = token;  // strdup(token);
 
         if (*(temp + i) == '\0')
             break;
 
-        i++;
+        //i++;
     }
     *(result + idx) = 0;
 
     return result;
 }
 
-int main() {
-    char **tokens = read_input_makefile("temp_file");
-    int i = 0;
-    while (*(tokens + i) != NULL) {
-        printf("%s\n", *(tokens + i));
-        free (*(tokens + i));
-        i++;
+char ** read_input_makefile (char *file_name) {
+    FILE * makefile_ptr = fopen(file_name, "r");
+
+    char ch = fgetc(makefile_ptr);
+    while (ch != EOF) {     // ((ch = fgetc(makefile_ptr)) != EOF) {
+        MakeNode * curr_node = NULL;
+        char * target_line = NULL;
+        // read a line: 
+        //   4 possibilities: target line, command (begins with \t), empty line, comment (TODO: bonus)
+        switch (ch) {
+            case '\t': {
+                // command
+
+                // read the whole line into a buffer
+                char * command_line = (char *) malloc(sizeof(char) * 256);   // TODO: Change it to MAX_LINE_LEN
+
+                int i = 0;
+                ch = fgetc(makefile_ptr);   // ignore the \t char
+                while (ch != EOF && ch != '\n' && ch != '\0') {
+                    *(command_line + i) = ch;
+                    i++;
+                    ch = fgetc(makefile_ptr);
+                }
+                *(command_line + i) = '\0';
+
+                fprintf(stdout, "Command: %s\n", command_line);
+
+                break;
+            }
+            case '\n': {
+                // empty line
+
+                // igmore all the consecutive empty lines
+                while (ch == '\n') {
+                    // fprintf(stdout, "Ignoring new line\n");
+                    ch = fgetc(makefile_ptr);
+                }
+
+                break;
+            }
+            case '#': {
+                // comment
+
+                break;
+            }
+            default: {
+                // target line
+
+                // read the whole line into a buffer
+                target_line = (char *) malloc(sizeof(char) * 256);   // TODO: Change it to MAX_LINE_LEN
+
+                int i = 0;
+                while (ch != EOF && ch != '\n' && ch != '\0') {
+                    *(target_line + i) = ch;
+                    i++;
+                    ch = fgetc(makefile_ptr);
+                }
+                *(target_line + i) = '\0';
+
+                fprintf(stdout, "Target: %s\n", target_line);
+                // return tokenize_string(target_line);
+
+                break;
+            }
+        }
+        // break;
     }
-    free (tokens);
+
+    fclose(makefile_ptr);
+    return NULL;
+}
+
+int main() {
+    // char ** tokens = tokenize_string("main.o: main.c main.h dep1.c");
+    // char ** tokens = read_input_makefile("test_inputs/Makefile");
+    // int i = 0;
+    // while (*(tokens + i) != NULL) {
+    //     printf("%s\n", *(tokens + i));
+    //     free (*(tokens + i));
+    //     i++;
+    // }
+    // free (tokens);
+    read_input_makefile("test_inputs/Makefile");
 
     return 0;
 }
