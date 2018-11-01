47512cb000da789d8c2b3391f4e3249d1946924b
Giri prasanna Mugunda krishnan <mugundakrish@giripras-work>
2018-10-31 21:36:13 -0500

    Some changes to spec graph

--- Assignments/Assignment_3/build_spec_graph.c
+++ Assignments/Assignment_3/build_spec_graph.c
@@ -11,57 +11,151 @@
 #include <stdlib.h>
 #include "build_spec_graph.h"
 // A structure to represent an adjacency list node 
-struct adjListGraphNode
+struct graph_adj_list_node
 {
     MakeNode *target;
-    struct adjListGraphNode* next;
+    struct graph_adj_list_node* next;
 };
   
 // A structure to represent an adjacency list 
-struct graphAdjList
+struct graph_adj_list
 { 
-    struct adjListGraphNode *head;
+    struct graph_adj_list_node *head;
 };
 
-struct directedAcyclicGraph 
+struct directed_acyclic_graph 
 { 
     int targets_and_dependencies;
-    MakeNode *graphHead; 
-    struct graphAdjList* dependencies;
+    //MakeNode *graph_head; 
+    struct graph_adj_list* dependencies;
 };
 
-struct adjListGraphNode* newAdjListNode(MakeNode* target_node) 
+struct graph_adj_list_node* new_adj_list_node(MakeNode* target_node) 
 { 
-    struct adjListGraphNode* newNode = (struct adjListGraphNode*)malloc(sizeof(struct adjListGraphNode)); 
-    newNode->target = target_node;
-    newNode->next = NULL;
-    return newNode; 
+    struct graph_adj_list_node* new_node = (struct graph_adj_list_node*)malloc(sizeof(struct graph_adj_list_node)); 
+    new_node->target = target_node;
+    new_node->next = NULL;
+    return new_node; 
 }
 
-struct directedAcyclicGraph* createGraph(int no_of_target_dependencies) 
+struct directed_acyclic_graph* create_graph(int no_of_target_dependencies) 
 { 
-    struct directedAcyclicGraph* DAG = (struct directedAcyclicGraph*) malloc(sizeof(struct directedAcyclicGraph));
-    DAG->targets_and_dependencies = no_of_target_dependencies;
+    struct directed_acyclic_graph* dag = (struct directed_acyclic_graph*) malloc(sizeof(struct directed_acyclic_graph));
+    dag->targets_and_dependencies = no_of_target_dependencies;
   
     // Create an array of adjacency lists 
-    DAG->dependencies = (struct graphAdjList*) malloc(no_of_target_dependencies * sizeof(struct graphAdjList));
+    dag->dependencies = (struct graph_adj_list*) malloc(no_of_target_dependencies * sizeof(struct graph_adj_list));
   
     // Initialize each adjacency list as empty
     int i; 
     for (i = 0; i < no_of_target_dependencies; ++i) 
-    	DAG->dependencies[i].head = NULL; 
+    	dag->dependencies[i].head = NULL; 
   
-    return DAG;
+    return dag;
 } 
 
-void addDependency(struct directedAcyclicGraph* DAG, MakeNode *target, MakeNode *dependency)
+void add_dependency(struct directed_acyclic_graph* dag, MakeNode* target, MakeNode* dependency)
 {
     // Add an edge from target to dependency 
-    struct adjListGraphNode* newNode = newAdjListNode(dependency);
-    //How to update pointers here?
+    struct graph_adj_list_node* target_node = new_adj_list_node(target);
+    struct graph_adj_list_node* dependency_node = new_adj_list_node(dependency);
+    int i;
+    int src_node = -1;
+    int dest_node = -1;
+
+    for (i=0; i<(dag->targets_and_dependencies); i++) {
+            if (dag->dependencies[i].head == target_node) {
+        	    src_node = i;
+        	    continue;
+            }
+            if (dag->dependencies[i].head == dependency_node) {
+        	    dest_node = i;
+        	    continue;
+            }
+    }
+
+    if (src_node == -1){
+        for (i=0; i<(dag->targets_and_dependencies); i++) {
+            if (dag->dependencies[i].head == NULL) {
+        	    dag->dependencies[i].head = target_node;
+        	    src_node = i;
+        	    break;
+            }
+        }
+    }
+
+    if (dest_node == -1){
+        for (i=0; i<(dag->targets_and_dependencies); i++) {
+            if (dag->dependencies[i].head == NULL) {
+        	    dag->dependencies[i].head = dependency_node;
+        	    dest_node = i;
+        	    break;
+            }
+        }
+    }
+	
+    printf ("\ntarget is : //%s// and destination is //%s// and src_node and dest_node are %d and %d", target->name, dependency->name, src_node, dest_node);
+    dependency_node->next = dag->dependencies[src_node].head;
+    dag->dependencies[src_node].head = dependency_node;
+}
+
+void print_graph(struct directed_acyclic_graph* dag)
+{
+    int i;
+    struct graph_adj_list_node* ptr = (struct graph_adj_list_node*) malloc(sizeof(struct graph_adj_list_node));
+    for (i = 0; i < dag->targets_and_dependencies; i++)
+    {	
+        printf("\nInside for loop for printing interation %d of %d", i, dag->targets_and_dependencies);
+	ptr = dag->dependencies[i].head;
+        printf("\n%d: ", i);
+        while (ptr)
+        {
+            printf("s-> %s", ptr->target->name);
+            ptr = ptr->next;
+        }
+        printf("->NULL\n");
+    }
 }
 
 int main() {
+	printf("\nEnter here");
+	MakeNode* n1 = (MakeNode *) malloc(sizeof(MakeNode));;
+	MakeNode* n2 = (MakeNode *) malloc(sizeof(MakeNode));;
+	MakeNode* n3 = (MakeNode *) malloc(sizeof(MakeNode));;
+	MakeNode* n4 = (MakeNode *) malloc(sizeof(MakeNode));;
+	char * str1 = (char *) malloc(sizeof(char)*50);
+	char * str2 = (char *) malloc(sizeof(char)*50);
+	char * str3 = (char *) malloc(sizeof(char)*50);
+	char * str4 = (char *) malloc(sizeof(char)*50);
+	str1 = "He was";
+	str2 = "She is";
+	str3 = "They were";
+	str4 = "We are";
+	n1->name = str1;
+	n2->name = str2;
+	n3->name = str3;
+	n4->name = str4;
+	
+	printf("\nAssigned Names here: Printing below:");
+	printf("\nn1 name is //%s//", n1->name);
+	printf("\nn2 name is //%s//", n2->name);
+	printf("\nn3 name is //%s//", n3->name);
+	printf("\nn4 name is //%s//", n4->name);
+	
+	struct directed_acyclic_graph *dag = (struct directed_acyclic_graph *) malloc(sizeof(struct directed_acyclic_graph));
+	printf("\nAllocated space for DAG:");
+	
+	dag = create_graph(4);
+
+	printf("\nCreatedDAG:");
+	printf("\n\nDAG is located at : %d", dag);
+	add_dependency(dag, n1, n2);
+	printf("\n\nDAG is located at : %d", dag);
+	add_dependency(dag, n1, n3);
+	printf("\n\nDAG is located at : %d", dag);
+	add_dependency(dag, n2, n4);
+
+	print_graph(dag);
 	return 0;
 }
 
