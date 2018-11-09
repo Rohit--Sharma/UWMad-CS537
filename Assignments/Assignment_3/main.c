fe7bf5aef4027592fa3c3909434367e049e1cf94
Rohit Kumar Sharma <rsharma@stark.cs.wisc.edu>
2018-11-09 16:35:48 -0600

    Now a target to be run can also be sent to the make tool

--- Assignments/Assignment_3/main.c
+++ Assignments/Assignment_3/main.c
@@ -21,20 +21,32 @@ const size_t MAX_LINE_LEN = 1024;
 const size_t HASH_TABLE_SIZE = 10000;
 const int debug = 0;
 
+typedef struct {
+	char *makefile_name;
+	char *target_token;
+} cmdline_options;
+
+cmdline_options *create_cmdline_args_struct (char *make, char *token) {
+	cmdline_options *result = (cmdline_options *) malloc(sizeof(cmdline_options));
+	result->makefile_name = make;
+	result->target_token = token;
+	return result;
+}
+
 //This takes care of the bonus part - to pass any name for makefile through input
-char *options_processor (int argc, char *argv[]) 
+cmdline_options *options_processor (int argc, char *argv[]) 
 {
 	char *makefile_path = NULL;
 	// if no argument passed, return makefile or Makefile in the same order whichever exists. else, throw an error
 	if (argc <= 1) {
 		makefile_path = "makefile";
 		if (access(makefile_path, F_OK) != -1) {
-			return makefile_path;
+			return create_cmdline_args_struct (makefile_path, NULL);
 		}
 		else {
 			makefile_path = "Makefile";
 			if (access(makefile_path, F_OK) != -1) {
-				return makefile_path;
+				return create_cmdline_args_struct (makefile_path, NULL);
 			}
 			else {
 				fprintf(stderr, "Error 'makefile' or 'Makefile' doesn't exist.\n");
@@ -45,6 +57,7 @@ char *options_processor (int argc, char *argv[])
 
 	extern char *optarg;
 	extern int optopt;
+	extern int optind;
 
 	int c;
 	while ((c = getopt(argc, argv, "f:")) != -1) {
@@ -53,6 +66,10 @@ char *options_processor (int argc, char *argv[])
 			case 'f':
 				if (optarg != NULL) {
 					makefile_path = optarg;
+					if (access(makefile_path, F_OK) == -1) {
+						fprintf(stderr, "Error no such file %s exists.\n", makefile_path);
+						return NULL;
+					}
 				}
 				break;
 			case '?':
@@ -65,29 +82,61 @@ char *options_processor (int argc, char *argv[])
 				return NULL;
 			default:
 				abort();
+				if (debug)
+					printf ("Default\n");
 				return NULL;
 		}
 	}
 
-	return makefile_path;
+	// Check if any targets are passed that are to be executed using non-opt args
+	if (optind + 1 < argc) {
+		if (debug)
+			fprintf(stdout, "Extra command line arguments passed. \nExiting...\n");
+		exit(1);
+		return NULL;
+	}
+	if (optind < argc) {
+		if (debug)
+			fprintf(stdout, "Target to be executed: %s", argv[optind]);
+		if (makefile_path != NULL) {
+			return create_cmdline_args_struct (makefile_path, argv[optind]);
+		}
+		else {
+			makefile_path = "makefile";
+			if (access(makefile_path, F_OK) != -1) {
+				return create_cmdline_args_struct (makefile_path, argv[optind]);
+			}
+			else {
+				makefile_path = "Makefile";
+				if (access(makefile_path, F_OK) != -1) {
+					return create_cmdline_args_struct (makefile_path, argv[optind]);
+				}
+				else {
+					fprintf(stderr, "Error 'makefile' or 'Makefile' doesn't exist.\n");
+					return NULL;
+				}
+			}
+		}
+	}
+	return create_cmdline_args_struct (makefile_path, NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	char *makefile_name = options_processor(argc, argv);
-	if (makefile_name == NULL) {
+	cmdline_options *makefile_struct = options_processor(argc, argv);
+	if (makefile_struct == NULL) {
 		fprintf(stderr, "Error: Specified Makefile not found.\n Usage: ./537make (or) ./537make -f <Makefile name>\nExiting...\n");
 		exit(1);
 	}
 
 	if (debug)
-		fprintf (stderr, "Makefile from cmdline: %s\n", makefile_name);
+		fprintf (stderr, "Makefile from cmdline: %s %s\n", makefile_struct->makefile_name, makefile_struct->target_token);
 
 	//Creating a hash table
 	hash_table *my_map = create_hash_table(HASH_TABLE_SIZE);
 
 	//Reads input make file and makes sense of the targets and dependencies
-	make_stats *make_file_stats = read_input_makefile(my_map, makefile_name);
+	make_stats *make_file_stats = read_input_makefile(my_map, makefile_struct->makefile_name);
 	if (debug)
 		fprintf(stdout, "Exiting read_input_makefile()\n");
 
@@ -103,7 +152,23 @@ int main(int argc, char *argv[])
 		fprintf(stdout, "Exiting construct_graph_edges()\n");
 		print_graph(dag);
 	}
-	int index_head = index_head_node(dag, make_file_stats->root);
+
+	char *root = NULL;
+	if (makefile_struct->target_token != NULL) {
+		root = makefile_struct->target_token;
+		free (makefile_struct);
+	}
+	else {
+		free (makefile_struct);
+		root = make_file_stats->root;
+	}
+
+	int index_head = root != NULL ? index_head_node(dag, root) : -1;
+	if (index_head == -1) {
+		fprintf (stderr, "Target %s not found in the makefile.\n Exiting...\n", root);
+		exit(1);
+	}
+
 	if (debug)
 	{
 		printf("Index of root: %d\n", index_head);
--- Assignments/Assignment_3/text_parsing.c
+++ Assignments/Assignment_3/text_parsing.c
@@ -364,7 +364,7 @@ make_stats *read_input_makefile(hash_table *map, char *file_name)
 	} while (line != NULL);
 
 	// If cmds_head is not null, create the last makenode
-	if (cmds_head != NULL)
+	if (target_line != NULL)
 	{
 		curr_node = create_node(target_line, cmds_head);
 		int num_children = num_dependencies(curr_node);
